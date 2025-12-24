1934cab2e0124640b5668b57166193c11349a26e
leonischu <nischal.201423@ncit.edu.np>
2025-12-24 16:20:07 +0545

    Add project files.

---
+++ AutomatedTransportEnquiry/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using AutomatedTransportEnquiry.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.OleDb;
+
+namespace AutomatedTransportEnquiry.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   
+    public class TestController : ControllerBase
+    {
+        private readonly DapperContext _context;
+        public TestController(DapperContext dapperContext)
+        {
+            
+            _context = dapperContext;
+
+        }
+
+        [HttpGet("db")]
+        public IActionResult TestDb()
+        {
+            using var connection = _context.CreateConnection();
+           // connection.Open();
+            return Ok("Database Connected Sucessfully!");
+        }
+    }
+}
---
+++ AutomatedTransportEnquiry/Data/DapperContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace AutomatedTransportEnquiry.Data
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+    }
+}
