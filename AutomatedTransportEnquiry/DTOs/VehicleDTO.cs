61c293737b5a11bab3e65be77829ad973d55028c
leonischu <nischal.201423@ncit.edu.np>
2025-12-26 16:17:38 +0545

    Vehicle Started

---
+++ AutomatedTransportEnquiry/DTOs/VehicleDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace AutomatedTransportEnquiry.DTOs
+{
+    public class VehicleDTO
+    {
+        public int VehicleId { get; set; }
+        public string VehicleNumber { get; set; }
+        public string VehicleType { get; set; }
+        public int Capacity { get; set; }
+
+    }
+
+    public class VehicleCreateDto
+    {
+        public string VehicleNumber { get; set; }
+        public string VehicleType { get; set; }
+        public int Capacity { get; set; }
+    }
+
+    public class VehicleUpdateDto
+    {
+        public int VehicleId { get; set; }
+        public string VehicleNumber { get; set; }
+        public string VehicleType { get; set; }
+        public int Capacity { get; set; }
+    }
+
+
+
+}
---
+++ AutomatedTransportEnquiry/Repositories/IVehicleRepository.cs
@@ -0,0 +1,14 @@
+﻿using AutomatedTransportEnquiry.Models;
+
+namespace AutomatedTransportEnquiry.Repositories
+{
+    public interface IVehicleRepository
+    {
+        Task<IEnumerable<Vehicle>> GetAllAsync();
+        Task<Vehicle> GetByIdAsync(int id);
+
+        Task<int>CreateAsync(Vehicle vehicle);  
+        Task<bool>UpdateAsync(Vehicle vehicle);
+        Task <bool>DeleteAsync(int id);
+    }
+}
