6926b05e4023d0bd5041a82c9a5bc4051843acb4
leonischu <nischal.201423@ncit.edu.np>
2025-12-30 14:28:52 +0545

    Search API Created

---
+++ AutomatedTransportEnquiry/Services/ISearchService.cs
@@ -0,0 +1,9 @@
+﻿using AutomatedTransportEnquiry.Models;
+
+namespace AutomatedTransportEnquiry.Services
+{
+    public interface ISearchService
+    {
+        Task<APIResponse>SearchAsync(string from, string to);   
+    }
+}
