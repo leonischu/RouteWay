edb004f845d8bd0f4d24a93fe63d22c033005aec
leonischu <nischal.201423@ncit.edu.np>
2026-01-02 17:30:48 +0545

    Ticket Cancellation Started

---
+++ AutomatedTransportEnquiry/DTOs/CancelBookingDto.cs
@@ -0,0 +1,17 @@
+﻿namespace AutomatedTransportEnquiry.DTOs
+{
+
+    public class CancelBookingDto
+    {
+        public int BookingId { get; set; }
+        public string? CancellationReason { get; set; }
+    }
+
+    public class CancelBookingResponseDto
+    {
+        public int BookingId { get; set; }
+        public string BookingStatus { get; set; }
+        public decimal RefundAmount { get; set; }
+        public string Message { get; set; }
+    }
+}
