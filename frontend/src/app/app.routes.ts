4ab7e97bb9fe3e733b98323b8e4dd2ede2457a39
leonischu <nischal.201423@ncit.edu.np>
2026-02-04 17:30:55 +0545

    View Profile section

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Register } from './auth/register/register';
 import { PageNotFound } from './pages/page-not-found/page-not-found';
 import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 import { ContactUs } from './pages/contact-us/contact-us';
+import { Account } from './pages/account/account';
 
 export const routes: Routes = [
 
@@ -13,6 +14,7 @@ export const routes: Routes = [
       { path: 'register', component: Register },
       { path: 'privacy', component: PrivacyPolicy },
       { path: 'contact', component: ContactUs },
+      { path: 'profile', component: Account },
       { path: "**", component:PageNotFound}
 
 ];
