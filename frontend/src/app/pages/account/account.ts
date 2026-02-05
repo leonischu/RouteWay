792fe98560859be628d5b63bb73086bd5fdf7f81
leonischu <nischal.201423@ncit.edu.np>
2026-02-05 11:53:09 +0545

    View Profile Page

--- frontend/src/app/pages/account/account.ts
+++ frontend/src/app/pages/account/account.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserDetail } from '../../model/userDetail';
 import { Auth } from '../../services/auth';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account',
@@ -11,10 +12,24 @@ import { CommonModule } from '@angular/common';
 })
 export class Account implements OnInit{
   user:UserDetail | null =null;
- constructor(private authService:Auth){}
+ constructor(private authService:Auth,private router: Router){}
+
+
  ngOnInit(): void {
-   this.authService.getUserProfile().subscribe(data => {
-    this.user = data;
-   });
+  this.authService.getUserProfile().subscribe({
+    next: (data) => {
+      console.log('Profile data received:', data);
+      this.user = data;
+    },
+    error: (err) => {
+      console.error('Error loading profile:', err);
+    }
+  });
+}
+
+ logout():void{
+  localStorage.removeItem('token');
+  this.user=null;
+  this.router.navigate(['/login'])
  }
 }
