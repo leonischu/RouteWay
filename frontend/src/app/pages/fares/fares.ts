d0719d61b4c4cf9a2ea57c298318f790aa683ef9
leonischu <nischal.201423@ncit.edu.np>
2026-02-11 11:19:15 +0545

    Fare Completed

--- frontend/src/app/pages/fares/fares.ts
+++ frontend/src/app/pages/fares/fares.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Api } from '../../services/api';
 import { Fare } from '../../model/Fare-Info';
 import { CommonModule } from '@angular/common';
@@ -22,7 +22,11 @@ export class Fares {
     price:0,
     routeId:0
    } ;
-constructor(private apiService :Api){}
+constructor(
+  private apiService :Api,
+  private cdr: ChangeDetectorRef
+
+){}
 ngOnInit():void{
   this.getFare();
   this.loadRoutes();
@@ -32,6 +36,7 @@ getFare():void{
   this.apiService.getFare().subscribe({
     next: (response) =>{
       this.fares = response.data;
+            this.cdr.detectChanges();        
     console.log(this.fares)
     },
       error: (err) => {
@@ -49,6 +54,8 @@ addFare():void {
     next:(response:Fare)=>{
        this.fares.push(response);
         this.newFare = { fareId: 0, routeName: '', price: 0, routeId: 0 }; // Reset the form after successful addition
+        console.log(this.newFare);
+              this.cdr.detectChanges();        
         alert('Fare added successfully!');
     }, error: (err) => {
         this.error = 'Failed to add fare'; 
@@ -60,7 +67,7 @@ loadRoutes(): void {
   this.apiService.getRoutes().subscribe({
     next: (res) => {
       this.routes = [...res.data];     
-      // this.cdr.detectChanges();      
+      this.cdr.detectChanges();      
       console.log(this.routes) 
     },
     error: (err) => console.error(err),
