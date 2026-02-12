8b85508b8149db1d7cccf9b911387d7f0f120cb0
leonischu <nischal.201423@ncit.edu.np>
2026-02-12 12:35:37 +0545

    Available Seat dto created .Left to add on frontend

--- frontend/src/app/model/schedule-info.ts
+++ frontend/src/app/model/schedule-info.ts
@@ -1,7 +1,7 @@
 export interface Schedule {
   scheduleId: number;
   vehicleId: number;
-  vehicleName: string;
+  
   routeId: number;
   travelDate: string | null;  
   departureTime: string;      
