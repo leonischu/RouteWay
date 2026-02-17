607b4716dd0b48fa2b4e51dc4523148dc22f1197
leonischu <nischal.201423@ncit.edu.np>
2026-02-17 11:11:20 +0545

    User Controller Created

---
+++ AutomatedTransportEnquiry/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using AutomatedTransportEnquiry.Models;
+using AutomatedTransportEnquiry.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutomatedTransportEnquiry.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    [ProducesResponseType(500)]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<APIResponse>> GetAll()
+        {
+            var response = await _service.GetAllAsync();
+            return StatusCode((int)response.StatusCode, response);
+
+        }
+
+    }
+}
---
+++ AutomatedTransportEnquiry/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using AutomatedTransportEnquiry.Models;
+using AutomatedTransportEnquiry.Repositories;
+using System.Net;
+
+namespace AutomatedTransportEnquiry.Services
+{
+    public class UserService : IUserService
+    {
+
+     
+        private readonly IUserRepository  _repository;
+        private readonly IMapper _mapper;
+        public UserService(IUserRepository repository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+
+
+        public async Task<APIResponse> GetAllAsync()
+        {
+            var response = new APIResponse();
+            try
+            {
+                var users = await _repository.GetAll();
+                response.Data = users;
+                response.Status = true;
+                response.StatusCode = HttpStatusCode.OK;
+
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.StatusCode = HttpStatusCode.InternalServerError;
+                response.Errors.Add(ex.Message);
+            }
+            return response;
+        }
+    }
+}
